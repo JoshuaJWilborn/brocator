7c8d09c33e65c03093df69aeca7fdb427bd9661e
Student <steve-india@appacademy.io>
2013-04-26 14:28:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brocator::Application.config.secret_token = '4b61bd0225781573cfa5cdecb9a7aa6b5d5ab49f94446b702daf60bcf834c0ae85336354c85db89f8461c6f3ca13e37053b74fa394a0634a88da4b180cfb4d39'
