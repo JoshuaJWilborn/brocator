f2cac5218f70d80e8458b222876f19366463059f
Student <steve-india@appacademy.io>
2013-04-26 16:54:24 -0700

    add provider and uid to users for omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130426220837) do
+ActiveRecord::Schema.define(:version => 20130426235333) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(:version => 20130426220837) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
