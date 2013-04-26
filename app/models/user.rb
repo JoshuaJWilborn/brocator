057d0969e908060f957acc94e697b447925cfdce
Student <steve-india@appacademy.io>
2013-04-26 15:15:26 -0700

    add paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :avatar
+
+	has_attached_file :avatar, :styles => {:medium => '300x300', :thumb => '100x100'},
+									  :default_url => '/images/:style/missing.png'
+	# attr_accessible :title, :body
 
 
 end
---
+++ db/migrate/20130426220837_add_avatars_to_users.rb
@@ -0,0 +1,6 @@
+class AddAvatarsToUsers < ActiveRecord::Migration
+  def change
+    add_attachment :users, :avatar
+
+	end
+end
