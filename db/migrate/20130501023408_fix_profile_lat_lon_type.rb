40f648baf944dc6c507a16ff5b6bdc6b760961ff
Student <steve-india@appacademy.io>
2013-05-01 18:17:53 -0700

    db

--- db/migrate/20130501023408_fix_profile_lat_lon_type.rb
+++ db/migrate/20130501023408_fix_profile_lat_lon_type.rb
@@ -1,9 +1,8 @@
 class FixProfileLatLonType < ActiveRecord::Migration
   def change
-		drop_column :profiles, :latitude
+		remove_column :profiles, :latitude
 		add_column :profiles, :latitude, :float
-    drop_column :profiles, :longitude
+    remove_column :profiles, :longitude
 		add_column :profiles, :longitude, :float
-
 	end
 end
