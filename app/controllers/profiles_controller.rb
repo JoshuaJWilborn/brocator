884b4c10997d5c31062e047a39ddb6cba468380a
Student <steve-india@appacademy.io>
2013-05-02 10:27:49 -0700

    fix profile creation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,6 +9,8 @@ class ProfilesController < ApplicationController
 	end
 
 	def create
+		params[:user][:avatar] = params[:profile][:avatar]
+    params[:profile].delete(:avatar)
     current_user.profile = Profile.new(params[:profile], :user_id => current_user.id)
 		current_user.update_attributes(params[:user])
 		current_user.save
@@ -22,7 +24,6 @@ class ProfilesController < ApplicationController
 	def update
 		params[:user][:avatar] = params[:profile][:avatar]
     params[:profile].delete(:avatar)
-		p params.inspect
 		current_user.profile.update_attributes(params[:profile], :user_id => current_user.id)
 	  current_user.update_attributes(params[:user])
 		redirect_to profile_path(params[:profile])
