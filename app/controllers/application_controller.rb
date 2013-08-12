140b2e38f6ebd0f70be5b6b5d6fecd163313f651
Joshua J Wilborn <Joshua.J.Wilborn@gmail.com>
2013-08-12 10:26:18 -0700

    Update application_controller.rb
    fix spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,14 @@
 
-
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-
-
-	include ProfilesHelper
+  include ProfilesHelper
 
   def after_sign_in_path_for(resource)
-		if current_user.profile
-			root_path
-		else
+    if current_user.profile
+      root_path
+    else
       new_profile_path
-		end
-	end
+    end
+  end
 end
