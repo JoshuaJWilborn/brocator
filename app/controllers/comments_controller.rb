f98ee657ca4aacb78f1733a3578fc211ad227243
Joshua J Wilborn <Joshua.J.Wilborn@gmail.com>
2013-08-12 10:27:45 -0700

    Update comments_controller.rb
    fix spacing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,15 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.new(params[:comment])
-		comment.user_id = current_user.id
-		comment.event_id = params[:event_id]
-		comment.save
-		redirect_to :back
-	end
+    comment.user_id = current_user.id
+    comment.event_id = params[:event_id]
+    comment.save
+    redirect_to :back
+  end
 
-	def destroy
-		@comment = Comment.find(params[:id])
-		if @comment.user == current_user
-			@comment.destroy
-		end
-		redirect_to :back
-	end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy if @comment.user == current_user
+    redirect_to :back
+  end
 end
