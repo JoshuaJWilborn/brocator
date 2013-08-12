adf5b76264468f811e1708f17d6c3f9b8ed95b4b
Joshua J Wilborn <Joshua.J.Wilborn@gmail.com>
2013-08-12 10:42:09 -0700

    Update users_controller.rb
    fix spacing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,13 @@
 class UsersController < ApplicationController
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
-	def index
-
-		@location = request.location.data
-		if @location['ip'] = "127.0.0.1"
-			@location['latitude'] = 37.78757258738848
-			@location['longitude'] = -122.39080927096823
-		end
-		@profiles = Profile.near([@location['latitude'], @location['longitude']], 25).
-		  includes(:fraternity)
-		@profiles.map!{|profile| profile.as_json(include: :fraternity)}
-	end
+  def index
+    @location = request.location.data
+    if @location['ip'] = "127.0.0.1"
+      @location['latitude'] = 37.78757258738848
+      @location['longitude'] = -122.39080927096823
+    end
+    @profiles = Profile.near([@location['latitude'], @location['longitude']], 25).includes(:fraternity)
+    @profiles.map!{|profile| profile.as_json(include: :fraternity)}
+  end
 end
