304cf7e68030dae6d7c13df92a939e7ff81b63d2
Joshua J Wilborn <Joshua.J.Wilborn@gmail.com>
2013-08-12 10:39:03 -0700

    Update events_controller.rb
    fix spacing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,90 +1,89 @@
 class EventsController < ApplicationController
   def index
-		@location = request.location.data
-		if @location['ip'] = "127.0.0.1"
-			@location['latitude'] = 37.78757258738848
-			@location['longitude'] = -122.39080927096823
-		end
-		@events = Event.near([@location['latitude'], @location['longitude']], 25).
-			includes(:fraternity)
-		@events.map!{|event| event.as_json(include: :fraternity)}
-	end
+    @location = request.location.data
+    if @location['ip'] = "127.0.0.1"
+      @location['latitude'] = 37.78757258738848
+      @location['longitude'] = -122.39080927096823
+    end
+ 
+    @events = Event.near([@location['latitude'], @location['longitude']], 25).includes(:fraternity)
+    @events.map!{|event| event.as_json(include: :fraternity)}
+  end
 
-
-	def search
-		if address = params[:address]
-			@location = {}
-			search = Geocoder.search("160 folsom st").first.data['geometry']['location']
-			@location['latitude'], @location['longitude'] = search['lat'], search['lng']
-			if distance = params[:distance]
+  def search
+    if address = params[:address]
+      @location = {}
+      search = Geocoder.search("160 folsom st").first.data['geometry']['location']
+      @location['latitude'], @location['longitude'] = search['lat'], search['lng']
+      if distance = params[:distance]
         @events = Event.near(address, distance.to_i)
-			  @events.map!{|event| event.as_json(include: :fraternity)}
-				render :index
-			else
+        @events.map!{|event| event.as_json(include: :fraternity)}
+	render :index
+      else
         @events = Event.near(address, 150)
-				@events.map!{|event| event.as_json(include: :fraternity)}
-				render :index
-			end
-		else
-			render :nothing => true
-		end
-	end
+	@events.map!{|event| event.as_json(include: :fraternity)}
+	render :index
+      end
+    else
+      render :nothing => true
+    end
+  end
 
-	def new
-		@event = Event.new
-	end
+  def new
+    @event = Event.new
+  end
 
-	def create
-		@event = Event.new(params[:event])
-		@event.user_id = current_user.id
-		if @event.save
-			redirect_to events_path(@event)
-		else
-			render :new
-		end
-	end
+  def create
+    @event = Event.new(params[:event])
+    @event.user_id = current_user.id
+    if @event.save
+      redirect_to events_path(@event)
+    else
+      render :new
+    end
+  end
 
-	def update
+  def update
     @event = Event.find(params[:id])
-		if @event.user_id == current_user.id
-			@event.update_attributes(params[:event])
-			@event.save
-			redirect_to event_path(@event)
-		else
+    if @event.user_id == current_user.id
+    @event.update_attributes(params[:event])
+    @event.save
+    redirect_to event_path(@event)
+    else
       redirect_to events_path
-		end
-	end
+    end
+  end
 
-	def edit
-		@event = Event.find(params[:id])
-		if current_user.id == @event.user.id
-			render :edit
-		else
-			render :show
-		end
-	end
-
-	def show
-		@event = Event.find(params[:id])
-		@comment = Comment.new
-	end
-
-	def destroy
+  def edit
     @event = Event.find(params[:id])
-		if current_user == @event.user
-			@event.destroy
-		end
-		redirect_to events_path
-	end
+    if current_user.id == @event.user.id
+      render :edit
+    else
+      render :show
+    end
+  end
+  
+  def show
+    @event = Event.find(params[:id])
+    @comment = Comment.new
+  end
 
-	def attend
-	  @event = Event.find(params[:id])
-	  if @event.attending_users.include?(current_user)
-			event_user = EventUser.where(:user_id => current_user.id, :event_id => @event.id)[0]
-		  event_user.destroy
-		else
-			EventUser.create!(:user_id => current_user.id, :event_id => @event.id)
-		end
-		redirect_to event_path(@event)
-	end
+  def destroy
+    @event = Event.find(params[:id])
+    if current_user == @event.user
+      @event.destroy
+    end
+    redirect_to events_path
+  end
+
+  def attend
+    @event = Event.find(params[:id])
+    if @event.attending_users.include?(current_user)
+      event_user = EventUser.where(:user_id => current_user.id, :event_id => @event.id)[0]
+      event_user.destroy
+    else
+      EventUser.create!(:user_id => current_user.id, :event_id => @event.id)
+    end
+    redirect_to event_path(@event)
+  end
 end
