4cb6539cbadeb11a0d87fe530a17b195f647536e
Joshua J Wilborn <Joshua.J.Wilborn@gmail.com>
2013-08-12 10:22:17 -0700

    Update profiles_controller.rb
    fixed tab spaces

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,41 +1,41 @@
 class ProfilesController < ApplicationController
 
-	def new
-	  if current_user.profile
-			redirect_to edit_profile_path(current_user.profile)
-		else
+  def new
+    if current_user.profile
+      redirect_to edit_profile_path(current_user.profile)
+    else
       @profile = Profile.new
-		end
-	end
+    end
+  end
 
-	def create
-		params[:user][:avatar] = params[:profile][:avatar]
+  def create
+    params[:user][:avatar] = params[:profile][:avatar]
     params[:profile].delete(:avatar)
     current_user.profile = Profile.new(params[:profile], :user_id => current_user.id)
-		current_user.update_attributes(params[:user])
-		current_user.save
-	  redirect_to root_path
-	end
+    current_user.update_attributes(params[:user])
+    current_user.save
+    redirect_to root_path
+  end
 
-	def edit
+  def edit
     @profile = current_user.profile
-	end
+  end
 
-	def update
-		params[:user][:avatar] = params[:profile][:avatar]
+  def update
+    params[:user][:avatar] = params[:profile][:avatar]
     params[:profile].delete(:avatar)
-		current_user.profile.update_attributes(params[:profile], :user_id => current_user.id)
-	  current_user.update_attributes(params[:user])
-		redirect_to profile_path(params[:profile])
-	end
+    current_user.profile.update_attributes(params[:profile], :user_id => current_user.id)
+    current_user.update_attributes(params[:user])
+    redirect_to profile_path(params[:profile])
+  end
 
-	def show
+  def show
     @profile = Profile.find(params[:id])
-	end
+  end
 
-	def search
-		first_name, last_name = params[:name].split(' ')
+  def search
+    first_name, last_name = params[:name].split(' ')
     @profile = Profile.where({:fname => first_name, :lname => last_name}).first
-		render :show
-	end
+    render :show
+  end
 end
